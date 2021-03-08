e4511861e20cccbc14ea2939f1ea152377eb43b2
agharaye emmanuel <agharayeemmanuel@hotmail.com>
2021-03-08 16:02:31 +0100

    Luhn algorithm obeyed

--- Services/AtmGenerator.cs
+++ Services/AtmGenerator.cs
@@ -1,7 +1,50 @@
+using System;
+using System.Linq;
+
 namespace Banking.Services
 {
     public class AtmGenerator
     {
         // This method generate a 16 digit atm number using luhm algorithms
+        public string AtmNumberGenerator(){
+            var random = new Random();
+            var secondRandom = new Random();
+            var secondrand = secondRandom.Next(100000, 9999999);
+            var rand = random.Next(100000000, 999999999);
+            var AtmNumber = "" + rand + secondrand;
+            return AtmNumber;
+        }
+
+        public string LuhnAlgorithm(string atmNumber)
+        {
+           // var CheckedAtmNumber = checkLuhnsAlgorithm(atmNumber);
+            
+            var correctValue = checkLuhnsAlgorithm(atmNumber)? atmNumber : default;
+            return correctValue;
+        }
+
+        public bool checkLuhnsAlgorithm(string atmNumber){
+            
+            int sum = 0;
+            bool alternate = false;
+            for (int i = atmNumber.Length - 1; i >= 0; i--)
+            {
+                char[] nx = atmNumber.ToArray();
+                int n = int.Parse(nx[i].ToString());
+
+                if (alternate)
+                {
+                    n *= 2;
+
+                    if (n > 9)
+                    {
+                        n = (n % 10) + 1;
+                    }
+                }
+                sum += n;
+                alternate = !alternate;
+            }
+            return (sum % 10 == 0);
+        }
     }
 }
---
+++ Services/Transactions.cs
@@ -0,0 +1,106 @@
+using System;
+
+namespace Banking.Services
+{
+    public class Transactions
+    {
+        public void Display(){
+            System.Console.WriteLine($"Press 1 to Open Account");
+            System.Console.WriteLine($"Press 2 to Withdraw");
+            System.Console.WriteLine($"Press 3 to Deposit");
+            System.Console.WriteLine($"Press 4 to Check Balance");
+            Console.WriteLine($"Press 5 to Manage Your Account");
+
+        }
+        public void OpenBankAccount(int input)
+        {
+            if (input == 1)
+            {
+                System.Console.WriteLine($"Welcome to Bobs Simple bank, thank you for banking with us");
+                //System.Console.WriteLine("Press 1 to open new account with us");
+                //var id = 1;
+                System.Console.WriteLine("Please enter First name");
+                var firstName = Console.ReadLine();
+                System.Console.WriteLine("Please enter Address");
+                var Address = Console.ReadLine();
+                System.Console.WriteLine("Please enter Contact Number ");
+                var ContactNumber = Console.ReadLine();
+                System.Console.WriteLine("Please enter Balance");
+                var balance = Console.ReadLine();
+                var convertedBalance = double.Parse(balance);
+                //AccountNumberGeneration ac = new AccountNumberGeneration();
+                var accountNumber = AccountNumberGeneration.AccoutNumberGenerator();
+                var acc = accountNumber;
+                System.Console.WriteLine($"Thanks you {firstName} for opening an account with us this is your account number {acc} you have {balance} in your account");
+
+                AddAccount addAccount = new AddAccount();
+                addAccount.OpenAccount(firstName, Address, ContactNumber, convertedBalance, acc);
+            }
+
+        }
+
+        public void Withdraw(int input)
+        {
+            if (input == 2)
+            {
+                var withdraw = new Withdrawal();
+                System.Console.WriteLine("please enter your name");
+                var user = Console.ReadLine();
+                System.Console.WriteLine("Please Enter the amount you want to Withdraw");
+                var amount = double.Parse(Console.ReadLine());
+                withdraw.AddWithdrawal(amount, user);
+            }
+
+        }
+        public void Deposit(int input)
+        {
+            if (input == 3)
+            {
+                var deposit = new Deposit();
+                System.Console.WriteLine("Please Enter your name");
+                var name = System.Console.ReadLine();
+                System.Console.WriteLine($"Please Enter the amount to be Deposited");
+                var amount = double.Parse(Console.ReadLine());
+                deposit.AddDeposit(amount, name);
+            }
+
+        }
+        public void CheckBalance(int input)
+        {
+            if (input == 4)
+            {
+                var balanceCheck = new BalanceCheck();
+                Console.WriteLine("Please Enter your Name");
+                var name = Console.ReadLine();
+                balanceCheck.CheckBalance(name);
+            }
+        }
+
+        public void ManageAccount(int input){
+             if (input == 5)
+            {
+
+                Console.WriteLine("Press 1 to Request for an ATM");
+                var selected = int.Parse(Console.ReadLine());
+                if (selected == 1)
+                {                   
+                    var atmGenerator = new AtmGenerator();
+                        
+                        while(true)
+                        {
+                            //var atm = atmGenerator.AtmNumberGenerator();
+                            var atmchecked = atmGenerator.AtmNumberGenerator();
+                            var luthCheckedAtm = atmGenerator.checkLuhnsAlgorithm(atmchecked);
+                            var verifyAtmNo = atmGenerator.LuhnAlgorithm(atmchecked);
+                            if(luthCheckedAtm == true){
+                                Console.WriteLine($"This is your Atm Number {atmchecked}");
+                                break;
+                            } 
+                            
+                        } 
+
+                }
+            }
+        }
+    }
+}
